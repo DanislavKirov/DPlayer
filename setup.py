14d5cb6958c1d8ec7ef626d91454ba9090d300b7
Danislav Kirov <danislav.kirov@gmail.com>
2016-05-20 12:45:57 +0300

    upload setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setup(
+    name='DPlayer',
+    version='0.1.0',
+    description='A music player',
+    long_description=long_description,
+    url='https://github.com/DanislavKirov/DPlayer',
+    author='Danislav Kirov',
+    author_email='danislav.kirov@gmail.com',
+    license='GNU GPL v2',
+    packages=['DPlayer'],
+    classifiers=[
+        'Development Status :: 1 - planning',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'Natural Language :: English',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Multimedia :: Sound/Audio :: Players :: MP3'
+    ],
+    keywords='music player'
+)
